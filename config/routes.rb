af467892b3e95eee0aece1b3eb90027c272d1ce6
Dale Pollitt <dale.negativezero@gmail.com>
2017-04-02 09:29:03 -0700

    build signup page, add signup functionality

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   get "about", to: "pages#about"
 
   resources :articles #basically add all the routes for routes automagically
+  resources :users, except: [:new]
+  get 'signup', to: 'users#new'
 
 end
