0bc8fd3008ca6e73c009ec9115157cbeeb09506e
Dale Pollitt <dale.negativezero@gmail.com>
2017-04-02 17:20:49 -0700

    add users listing page and show article view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,12 +26,15 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+  def index
+    @users = User.all
+  end
 
   private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
-  
+
 
 end
