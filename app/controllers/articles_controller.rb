5c5ef877e6e0dfe5918206592fb9bad0e4ec7304
Dale Pollitt <dale.negativezero@gmail.com>
2017-04-15 14:15:50 -0700

    complete authentication methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
-
+  before_action :require_user, except: [:index, :show]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
   def index
     @articles = Article.paginate(page: params[:page], per_page: 5)
   end
@@ -15,8 +16,7 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-    #hard coded for now
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:success] = "Article Successfully Created"
       redirect_to article_path(@article)
@@ -52,5 +52,12 @@ class ArticlesController < ApplicationController
 
   def set_article
     @article = Article.find(params[:id])
+  end
+
+  def require_same_user
+    if current_user != @article.user
+      flash[:danger] = "You can only edit or delete your own article"
+      redirect_to root_path
+    end
   end
 end
