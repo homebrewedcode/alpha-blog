fd40903d0d2f632f448618a72eb1b1ee9e22fb7c
Dale Pollitt <dale.negativezero@gmail.com>
2017-04-15 14:58:15 -0700

    add admin user functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :articles
+  has_many :articles, dependent: :destroy #this dependent destroy cascades the deletion
   before_save { self.email = email.downcase }
   validates :username, presence: true,
             uniqueness: { case_sensitive: false },
