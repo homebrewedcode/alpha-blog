e90a85567d8a90dc48877ab837b324ca1e17621f
Dale Pollitt <dale.negativezero@gmail.com>
2017-04-14 19:45:14 -0700

    Created controller and views to create/destroy user session

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,9 @@ Rails.application.routes.draw do
   resources :articles #basically add all the routes for routes automagically
   resources :users, except: [:new]
   get 'signup', to: 'users#new'
+  
+  get 'login', to: "sessions#new"
+  post 'login', to: "sessions#create"
+  delete 'logout', to: "sessions#destroy"
 
 end
